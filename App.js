f9765ac605036ffe778eed5187d61bcd5fc70d08
dmilagros <dmilagrosvelasquez@gmail.com>
2022-08-07 23:46:49 -0500

    29/06 | CLASE 03: Core Components

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Ingese aquí" style={styles.input} />
+        <Button title="ADD" onPress={()=> {}} />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  screen: { padding: 30 },
+  inputContainer: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' },
+  input: { borderBottomColor: 'black', borderBottomWidth: 1, width: '80%', height: 50 }
 });
